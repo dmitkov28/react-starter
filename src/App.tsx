aceafd01015453d519406054184d2cc2d08231f0
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-05-16 12:11:47 +0300

    style: enhance styling for header and counter components

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Counter from "./Counter";
 function App() {
   return (
     <>
-      <h1>Hello, world!</h1>
+      <h1 className="text-3xl font-bold text-center my-6">Hello, world!</h1>
       <Counter />
     </>
   );
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -15,10 +15,20 @@ export default function Counter() {
 
   return (
     <div>
-      <h2>Count: {count}</h2>
-      <div>
-        <button onClick={decrement}>-</button>
-        <button onClick={increment}>+</button>
+      <h2 className="text-center text-2xl font-bold">Count: {count}</h2>
+      <div className="grid grid-cols-2 gap-4 max-w-[300px] mx-auto my-8">
+        <button
+          className="col-span-2 md:col-span-1 border border-gray-400 shadow-md rounded-md"
+          onClick={decrement}
+        >
+          -
+        </button>
+        <button
+          className="col-span-2 md:col-span-1 border border-gray-400 shadow-md rounded-md"
+          onClick={increment}
+        >
+          +
+        </button>
       </div>
     </div>
   );
